4df1371626de5d1301c8508a5e05ea55bf07795d
guoyha <guoyha@adtec.com.cn>
2020-12-30 19:45:49 +0800

    swagger

--- springbootmybatis/src/main/java/com/example/springbootmybatis/service/UserService.java
+++ springbootmybatis/src/main/java/com/example/springbootmybatis/service/UserService.java
@@ -8,55 +8,47 @@ import java.util.List;
 public interface UserService {
 
     /**
-     *
      * @return
      */
     List<UserEntity> findAll();
 
     /**
-     *
      * @param id
      * @return
      */
     UserEntity findUserById(int id);
 
     /**
-     *
      * @param userEntity
      * @return
      */
     boolean insertUser(UserEntity userEntity);
 
     /**
-     *
      * @param name
      * @return
      */
     UserEntity findUserByName(String name);
 
     /**
-     *
      * @param userEntity
      * @return
      */
     boolean updateUser(UserEntity userEntity);
 
     /**
-     *
      * @param id
      * @return
      */
     boolean deleteById(int id);
 
     /**
-     *
      * @param name
      * @return
      */
     List<UserEntity> findUserLikeName(String name);
 
     /**
-     *
      * @param ids
      * @return
      */
