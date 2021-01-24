760755a7cff175c52f51933aa879535badc87524
guoyha <guoyha@adtec.com.cn>
2021-01-24 16:42:03 +0800

    JPA REST

--- springbootjparest/src/main/java/com/example/springbootjparest/dao/BookRepository.java
+++ springbootjparest/src/main/java/com/example/springbootjparest/dao/BookRepository.java
@@ -5,18 +5,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 /**
  * 自定义访问路径
+ * 全局跨域
  */
+@CrossOrigin
 @RepositoryRestResource(path = "bs", collectionResourceRel = "bs", itemResourceRel = "b")
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
     /**
+     * 仅限该方法跨域
      * 自定义访问路径
      */
+    @CrossOrigin
     @RestResource(path = "author", rel = "author")
     List<Book> findByAuthorContains(@Param(("author")) String author);
 
